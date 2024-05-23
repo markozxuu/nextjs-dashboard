f7caa2792202438b9dca5510365e780007891b62
Markoz Peña <markdrew53@gmail.com>
2024-05-23 15:16:48 -0600

    feat: add noStore cache

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,5 @@
 import { sql } from '@vercel/postgres';
-import { unstable_cache as noStore } from 'next/cache';
+import { unstable_cache as noStore } from 'next/cache'
 import {
   CustomerField,
   CustomersTableType,
@@ -20,12 +20,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
