d64d6ddea6d414d18bdc95bfcac6fb014adb0e6f
Markoz Peña <markdrew53@gmail.com>
2024-08-10 21:57:18 -0600

    feat: create debunci

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,6 +9,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
 	const { replace } = useRouter();
 
 	const handleSearch = (term: string) => {
+		console.log(`Searching... ${term}`);
+
 		const params = new URLSearchParams(searchParams);
 
 		if (term) {
@@ -25,6 +27,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 				Search
 			</label>
 			<input
+				defaultValue={searchParams.get("query")?.toString() ?? ""}
 				onChange={(e) => handleSearch(e.target.value)}
 				className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
 				placeholder={placeholder}
