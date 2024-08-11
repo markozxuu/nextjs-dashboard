97213d5615129dd3a123e54523632dca19ed1b1c
Markoz Peña <markdrew53@gmail.com>
2024-08-10 22:12:12 -0600

    feat: add debucing feature

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useDebounce } from "use-debounce";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
 
@@ -8,7 +8,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 	const pathName = usePathname();
 	const { replace } = useRouter();
 
-	const handleSearch = (term: string) => {
+	const handleSearch = useDebounce((term: string) => {
 		console.log(`Searching... ${term}`);
 
 		const params = new URLSearchParams(searchParams);
@@ -20,7 +20,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
 		}
 
 		replace(`${pathName}?${params.toString()}`);
-	};
+	}, 300);
+
 	return (
 		<div className="relative flex flex-1 flex-shrink-0">
 			<label htmlFor="search" className="sr-only">
